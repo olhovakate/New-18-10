370e3cdf02eca5166ca57fc8f88f37fbf705b9a9
Kate <eolhovaa5@gmail.com>
2023-10-19 18:49:00 +0200

    average score for student

---
+++ task_score.js
@@ -0,0 +1,10 @@
+let introScore = 85;
+let gitScore = 90;
+let jsScore = 78;
+let averageScore = (introScore + gitScore + jsScore) / 3;
+let firstName = "Kate";
+let lastName = "Ivanova";
+console.log(
+  `Студент: ${firstName} ${lastName}`,
+  `Средний балл: ${averageScore}`
+);
