617f8b970d63b3e9b110f74b30facad5b0af5cd4
Kate <eolhovaa5@gmail.com>
2023-10-19 17:31:15 +0200

    dynamic type

---
+++ JS.js
@@ -0,0 +1,9 @@
+let myData = "Kate";
+let myName = "Kateryna";
+let fullData = myData + myName;
+console.log(fullData);
+
+let myData = 40;
+let myName = "Kateryna";
+let fullData = myName + myData;
+console.log(fullData);
