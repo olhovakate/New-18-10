c81b19160014fca850001d4a33efc042654dec8a
Kate <eolhovaa5@gmail.com>
2023-10-19 23:38:13 +0200

    slice metod HW 2.1.4

--- task_score.js
+++ task_score.js
@@ -4,7 +4,8 @@ let jsScore = 78;
 let averageScore = (introScore + gitScore + jsScore) / 3;
 let firstName = "Kate";
 let lastName = "Ivanova";
+let firstName1 = firstName.slice(0, 1);
 console.log(
-  `Студент: ${firstName} ${lastName}`,
+  `Студент: ${firstName1}. ${lastName}`,
   `Средний балл: ${averageScore}`
 );
